ace69fe4fb82fed44d127659bb3c583e3f77d824
Fanny <fannybecerravera@gmail.com>
2018-05-21 16:54:29 -0400

    Agregando botones de si y no para preguntar si quiere jugar en index.html y app.js

--- 2018-1-Laboratoria-Trivia/app.js
+++ 2018-1-Laboratoria-Trivia/app.js
@@ -4,3 +4,17 @@ function toReceive()
     document.getElementById("txt").innerHTML= " ¡Hola " + name +"!" + ", ¿quieres jugar?";        
     
 }        
+
+function play1()
+    {
+        var yesIWant = document.getElementById("written1").value;
+        document.getElementById("written1").innerHTML= " ¡Comencemos a jugar!. Pasa a las preguntas." ;        
+        
+    } 
+    
+function play2()
+    {
+        var iDoNotWant = document.getElementById("written1").value;
+        document.getElementById("written1").innerHTML= "Jugaremos en otra oportunidad." ;        
+        
+    } 
